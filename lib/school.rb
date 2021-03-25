8cbb85e89bfe4925d83b554bd73d5e1e2067d94d
Your Username <you@example.com>
2021-03-25 08:22:43 -0500

    commit tree 7978e7371e278ec2dafd874795f90fb41225c9ab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require "pry"
+
+class School
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end
+
+    def name
+        @name 
+    end
+
+    def roster=(roster)
+        @roster = roster 
+    end
+
+    def roster
+        @roster
+    end
+
+   
+end 
+ 
+school = School.new("Bayside High School")
+ binding.pry
